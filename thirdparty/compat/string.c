fa98d7326f808cdf90c4c3ecaf31beb152f099cd
OmniBlade <nadia_xy@hotmail.com>
2021-03-23 22:36:46 +0000

    Adds compat library.
    Small lib covering differences between C runtimes for common
    functionality and adds some useful cross platform intrinsics.

---
+++ thirdparty/compat/string.c
@@ -0,0 +1,152 @@
+/**
+ * @file
+ *
+ * @author OmniBlade
+ *
+ * @brief Some extra string manipulation functions not present in all standard CRTs
+ *
+ * @copyright StdCompat is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#include "string.h"
+
+#ifndef HAVE_STRTRIM
+char *strtrim(char *str)
+{
+    size_t len = 0;
+    char *frontp = str;
+    char *endp = NULL;
+
+    if (str == NULL) {
+        return NULL;
+    }
+
+    if (str[0] == '\0') {
+        return str;
+    }
+
+    len = strlen(str);
+    endp = str + len;
+
+    /* Move the front and back pointers to address the first non-whitespace
+     * characters from each end.
+     */
+    while (isspace((unsigned char)*frontp)) {
+        ++frontp;
+    }
+
+    if (endp != frontp) {
+        while (isspace((unsigned char)*(--endp)) && endp != frontp) {
+        }
+    }
+
+    if (str + len - 1 != endp) {
+        *(endp + 1) = '\0';
+    } else if (frontp != str && endp == frontp) {
+        *str = '\0';
+    }
+
+    /* Shift the string so that it starts at str so that if it's dynamically
+     * allocated, we can still free it on the returned pointer.  Note the reuse
+     * of endp to mean the front of the string buffer now.
+     */
+    endp = str;
+    if (frontp != str) {
+        while (*frontp) {
+            *endp++ = *frontp++;
+        }
+
+        *endp = '\0';
+    }
+
+    return str;
+}
+#endif
+
+#ifndef HAVE_STRLWR
+char *strlwr(char *str)
+{
+    char *p = str;
+
+    while (*p) {
+        *p = tolower((unsigned char)*p);
+        p++;
+    }
+
+    return str;
+}
+#endif
+
+#ifndef HAVE_STRUPR
+char *strupr(char *str)
+{
+    char *p = str;
+
+    while (*p) {
+        *p = toupper((unsigned char)*p);
+        p++;
+    }
+
+    return str;
+}
+#endif
+
+#ifndef HAVE_STRLCAT
+size_t strlcat(char *dst, const char *src, size_t dsize)
+{
+    const char *odst = dst;
+    const char *osrc = src;
+    size_t n = dsize;
+    size_t dlen;
+
+    /* Find the end of dst and adjust bytes left but don't go past end. */
+    while (n-- != 0 && *dst != '\0')
+        dst++;
+    dlen = dst - odst;
+    n = dsize - dlen;
+
+    if (n-- == 0)
+        return (dlen + strlen(src));
+    while (*src != '\0') {
+        if (n != 0) {
+            *dst++ = *src;
+            n--;
+        }
+        src++;
+    }
+    *dst = '\0';
+
+    return (dlen + (src - osrc)); /* count does not include NUL */
+}
+#endif
+
+
+#ifndef HAVE_STRLCPY
+size_t strlcpy(char *dst, const char *src, size_t dsize)
+{
+    const char *osrc = src;
+    size_t nleft = dsize;
+
+    /* Copy as many bytes as will fit. */
+    if (nleft != 0) {
+        while (--nleft != 0) {
+            if ((*dst++ = *src++) == '\0')
+                break;
+        }
+    }
+
+    /* Not enough room in dst, add NUL and traverse rest of src. */
+    if (nleft == 0) {
+        if (dsize != 0)
+            *dst = '\0'; /* NUL-terminate dst */
+        while (*src++)
+            ;
+    }
+
+    return (src - osrc - 1); /* count does not include NUL */
+}
+#endif
